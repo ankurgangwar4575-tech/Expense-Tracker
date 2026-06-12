46e038b5c2c5d8b5779d95ae1b9781f9d95f56e0
Ankur Gangwar <ankurgangwar4575@gmail.com>
2026-06-12 13:39:24 +0530

    Utility

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,13 @@
-const app=require('./src/app.js');
-const connectDB=require("./src/db/db.js");
-const PORT=process.env.PORT || 3000;
+const app = require("./src/app.js");
+const connectDB = require("./src/db/db.js");
+const PORT = process.env.PORT || 3000;
 
-connectDB();
-app.listen(PORT,(req,res)=>{
-    console.log("Server is running on port : ",PORT);
-});
+connectDB()
+  .then(() => {
+    app.listen(PORT, (req, res) => {
+      console.log("Server is running on port :", PORT);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB Connection error: ", error.message);
+  });
--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,5 +1,7 @@
-require('dotenv').config();
-const express=require('express');
-const app=express();
+require("dotenv").config();
+const express = require("express");
+const app = express();
 
-module.exports=app;
+
+
+module.exports = app;
